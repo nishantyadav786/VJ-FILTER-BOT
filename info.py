853dcae7bbf19f2876fc3e438e847c8e40a7d826
nishantyadav786 <khalnayaknishant07@gmail.com>
2024-10-28 22:31:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() #SAMPLE PIC
+PICS = (environ.get('PICS', 'https://envs.sh/A-D.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/ce1723991756e48c35aa1.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/ce1723991756e48c35aa1.jpg")
