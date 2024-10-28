6441cfc74e237c9dd10f5106181087ccd0c854a4
nishantyadav786 <khalnayaknishant07@gmail.com>
2024-10-28 23:09:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+Xr3J31tNqEEyNTVl')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Ni_Movie_Request_Group')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Ni_Backup_2')
 TUTORIAL = environ.get('TUTORIAL', '')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Ni_Backup_2') # Support Chat Link Without https:// or @
